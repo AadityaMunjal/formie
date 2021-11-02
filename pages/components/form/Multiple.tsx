e642683f9d6628a843cd8c5cc1dbe1bf3360f091
Anurag <kr.anurag24@gmail.com>
2021-11-02 15:04:12 +0530

    initial commit

--- pages/components/form/Multiple.tsx
+++ pages/components/form/Multiple.tsx
@@ -1,12 +1,42 @@
-import { RadioGroup, FormLabel, Radio } from "@chakra-ui/react";
+import { Input, RadioGroup, FormLabel, Radio } from "@chakra-ui/react";
+
+import { useState } from "react";
 
 export default function Multiple() {
+  let [opt, setOpt] = useState("");
+  let [options, setOptions] = useState([]);
+
+  console.log(options);
+  const addOption = () => {
+    setOptions([...options, opt]);
+  };
   return (
     <RadioGroup className="flex flex-col my-4 text-xl">
-      <FormLabel> Who is ur fav Uwu? </FormLabel>
-      <Radio value="1">Harry Potter</Radio>
+      <FormLabel>
+        <Input value="Who is ur fav UwU?" variant="flushed" size="md" />
+      </FormLabel>
+      <p className="flex flex-row items-center">
+        <Input
+          variant="flushed"
+          size="sm"
+          value={opt}
+          onChange={(e) => setOpt(e.target.value)}
+        />
+        <button
+          className="mx-4 p-2 bg-purple-600 rounded text-white font-medium focus:ring-4"
+          onClick={addOption}
+        >
+          {" "}
+          Add{" "}
+        </button>
+      </p>
+      {/* <Radio value="1">Harry Potter</Radio>
       <Radio value="2">Ronald</Radio>
-      <Radio value="3">Nope</Radio>
+      <Radio value="3">Nope</Radio> */}
+
+      {options.map((d) => {
+        return <Radio value={d}> {d} </Radio>;
+      })}
     </RadioGroup>
   );
 }
