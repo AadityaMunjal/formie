3c943ce38d50de1bc290568ad919e74fc317f484
Anurag <kr.anurag24@gmail.com>
2021-11-03 11:19:50 +0530

    initial commit

--- pages/components/form/Multiple.tsx
+++ pages/components/form/Multiple.tsx
@@ -2,29 +2,34 @@ import { Input, RadioGroup, FormLabel, Radio } from "@chakra-ui/react";
 
 import { useState } from "react";
 
-export default function Multiple() {
+export default function Multiple({ onChange }) {
   let [opt, setOpt] = useState("");
   let [options, setOptions] = useState([]);
 
-  const addOption = () => {
+  const addOptions = () => {
     setOptions([...options, opt]);
   };
+
   return (
     <RadioGroup className="flex flex-col my-4 text-xl">
       <FormLabel>
-        <Input variant="flushed" size="lg" placeholder="Question goes brr..." />
+        <Input
+          variant="flushed"
+          size="lg"
+          placeholder="Question goes brr..."
+          onChange={onChange}
+        />
       </FormLabel>
       <p className="flex flex-row items-center">
         <Input
           variant="flushed"
           size="sm"
-          value={opt}
           onChange={(e) => setOpt(e.target.value)}
           placeholder="Add an option"
         />
         <button
           className="mx-4 p-2 bg-purple-600 rounded text-white font-medium focus:ring-4"
-          onClick={addOption}
+          onClick={addOptions}
         >
           {" "}
           Add{" "}
