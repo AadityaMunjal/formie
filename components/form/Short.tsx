54ec0c600537201765e64fcacb2df1fd81cba868
Anurag <kr.anurag24@gmail.com>
2021-11-07 19:01:59 +0530

    WIP

--- components/form/Short.tsx
+++ components/form/Short.tsx
@@ -1,22 +1,31 @@
 import { FormControl, Input } from "@chakra-ui/react";
 
-export default function Short({ short, setShort }) {
+export default function Short({ short }) {
   return (
     <>
+      <div className="w-full flex justify-center">
+        <button
+          className="my-2 mb-4 px-6 py-2 rounded font-medium text-white bg-red-600 focus:ring-2 ring-red-300"
+          onClick={null}
+        >
+          Delete
+        </button>
+      </div>
+
       <FormControl isRequired>
         <Input
           placeholder="Question goes brr..."
           className="font-poppins font-medium m-2"
           size="lg"
           variant="flushed"
-          onChange={setShort}
           value={short}
         />
 
         <Input
-          placeholder="Answer goes brr.."
+          placeholder="Answer goes brr..."
           className="font-poppins font-medium m-2"
           size="md"
+          isDisabled
         />
       </FormControl>
     </>
