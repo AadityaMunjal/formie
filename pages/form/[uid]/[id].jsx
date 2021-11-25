10f7782aff77ace41e7a88d85ac126e3b8999327
Anurag <kr.anurag24@gmail.com>
2021-11-25 12:09:50 +0530

    [WIP] major update

--- pages/form/[uid]/[id].jsx
+++ pages/form/[uid]/[id].jsx
@@ -17,30 +17,39 @@ function Form() {
   let [desc, setDesc] = useState("");
   let [questions, setQuestions] = useState([]);
 
-  let [answers, setAnswers] = useState([]);
+  let [answers, setAnswers] = useState([{ id: Number, val: String }]);
+  console.log(answers);
 
   const set = (id, e) => {
-    setAnswers([
-      ...answers,
-      {
-        id: id,
-        text: e.target.value,
-      },
-    ]);
+    answers.map((answer) => {
+      if (answer.id === id) {
+        answer.val = e.target.value;
+      }
+    });
   };
 
   useEffect(() => {
     async function getData() {
       const ref = doc(db, "users", uid);
       const docSnap = await getDoc(ref);
-      
+
       docSnap ? console.log(docSnap.data()) : console.log("no data");
 
       console.log(docSnap.data().forms[id]);
-      let form = docSnap.data().forms[id]
+      let form = docSnap.data().forms[id];
 
-      setTitle(form.title)
-      setQuestions(form.questions)
+      setTitle(form.title);
+      setQuestions(form.questions);
+
+      questions.map((q) => {
+        setAnswers([
+          ...answers,
+          {
+            id: q.id,
+            val: "",
+          },
+        ]);
+      });
     }
 
     getData();
@@ -49,13 +58,13 @@ function Form() {
   return (
     <div>
       <Title title="sup" />
-      
-      <div className="m-6 w-6/12 px-8 py-6 bg-white rounded">
-      <p className="text-3xl font-poppins font-medium text-gray-700">
-      {title}
-      </p>
-      </div>
 
+      <div className="m-6 w-6/12 px-8 py-6 bg-white rounded">
+        <p className="text-3xl font-poppins font-medium text-gray-700">
+          {title}
+        </p>
+      </div>
+      <button onClick={() => console.log(answers)}>click me</button>
       {questions.map((question) => (
         <Short
           key={question.id}
