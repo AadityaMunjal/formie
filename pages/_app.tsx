72d252f222ea7b2da4503a433ed2d651b9dca1e8
avneesh0612 <avneeshagarwal0612@gmail.com>
2021-11-28 11:54:39 +0530

    feat: added router progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from "next/app";
 import "../styles/globals.scss";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <NextNProgress color="#5A00FE" options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </ChakraProvider>
   );
